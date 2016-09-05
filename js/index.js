5eb4a4224ea60e85ec1602d68ac51551322b3808
Marc Shapiro <marcnshap@gmail.com>
2016-09-04 19:58:25 -0700

    Initial copy from CodePen

---
+++ js/index.js
@@ -0,0 +1,37 @@
+var text = "";
+       
+$(document).ready( function() {
+  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
+    var html = "";
+        
+    html += "<h2>" + json.quoteText + "</h2>"
+    html += "<br />"
+    html += "<p> -- " + json.quoteAuthor + "</p>"
+          
+    $("#quoteText").html(html);
+    text = document.getElementById("quoteText").textContent;
+  });  
+
+  $("#getQuote").on("click", function() {
+
+      $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
+        var html = "";
+ 
+        html += "<h2>" + json.quoteText + "</h2>";
+        html += "<br />";
+        
+        if (json.quoteAuthor !== "") {
+          html += "   -- " + json.quoteAuthor;
+        } else {
+          html += "   -- Unknown"
+        }
+          
+        $("#quoteText").html(html);      
+        text = document.getElementById("quoteText").textContent;
+      });   
+    });    
+});
+
+$("a#tweetQuote").click(function() {
+	window.open("https://twitter.com/intent/tweet?text=" + text , " " , "width=550,height=450");
+});
